c44e87bdedd7df046dea0141def35c5fd97508df
doubleh86 <doubleh86@gmail.com>
2025-07-30 13:27:07 +0900

    우편함 모듈 및 핸들러 추가

--- ApiServer/GameService/GameModules/MailModule.cs
+++ ApiServer/GameService/GameModules/MailModule.cs
@@ -2,6 +2,7 @@ using ApiServer.GameService.Models;
 using DbContext.Common.Models;
 using DbContext.MainDbContext.DbResultModel.GameDbModels;
 using DbContext.MainDbContext.SubContexts;
+using ServerFramework.CommonUtils.DateTimeHelper;
 using ServerFramework.SqlServerServices.Models;
 
 namespace ApiServer.GameService.GameModules;
@@ -9,19 +10,58 @@ namespace ApiServer.GameService.GameModules;
 public class MailModule : BaseModule<MailDbContext>, IGameModule
 {
     public long AccountId { get; set; }
-    private List<MailInfoDbResult> _mailLists = null;
+    private Dictionary<long, MailInfoDbResult> _mailLists = null;
     public MailModule(long accountId, SqlServerDbInfo masterDbInfo, SqlServerDbInfo slaveDbInfo) : base(masterDbInfo, slaveDbInfo)
     {
         AccountId = accountId;
     }
 
-    public async Task<List<MailInfoDbResult>> GetMailListAsync()
+    public async Task<Dictionary<long, MailInfoDbResult>> GetMailListAsync()
     {
         if (_mailLists != null)
             return _mailLists;
 
         var dbContext = GetDbContext(true);
-        _mailLists = await dbContext.GetMailInfoDbResultAsync(AccountId);
+        var dbResult = await dbContext.GetMailInfoDbResultAsync(AccountId);
+        if (dbResult == null)
+        {
+            _mailLists = new Dictionary<long, MailInfoDbResult>();
+            return _mailLists;
+        }
+
+        _mailLists = dbResult.ToDictionary(x => x.mail_uid);
         return _mailLists;
+    }
+
+    public async Task<List<long>> ReceiveMailRewardAsync(List<long> mailUidList)
+    {
+        var mailList = await GetMailListAsync();
+        var receivedFailed =  new List<long>();
+        var currentServerTime = TimeZoneHelper.ServerTimeNow;
+        
+        foreach (var mailUid in mailUidList)
+        {
+            if (mailList.TryGetValue(mailUid, out var mailDbInfo) == false)
+            {
+                receivedFailed.Add(mailUid);
+                continue;
+            }
+
+            if (mailDbInfo.expiry_date.ToServerTime() > currentServerTime)
+            {
+                receivedFailed.Add(mailUid);
+                continue;
+            }
+
+            if (mailDbInfo.is_reward_received == 1)
+            {
+                receivedFailed.Add(mailUid);
+                continue;
+            }
+            
+            mailDbInfo.is_reward_received = 0;
+        }
+        
+        return receivedFailed;
     }
 }
--- ApiServer/GameService/Handlers/GameHandlers/MailHandler.cs
+++ ApiServer/GameService/Handlers/GameHandlers/MailHandler.cs
@@ -1,6 +1,7 @@
 using ApiServer.GameService.GameModules;
 using ApiServer.Services;
 using DbContext.MainDbContext.DbResultModel.GameDbModels;
+using ServerFramework.CommonUtils.DateTimeHelper;
 using ServerFramework.SqlServerServices.Models;
 
 namespace ApiServer.GameService.Handlers.GameHandlers;
@@ -17,6 +18,42 @@ public class MailHandler(long accountId, ApiServerService serverService) : BaseH
     public async Task<List<MailInfoDbResult>> GetMailListAsync()
     {
         var mailModule = GetModule<MailModule>();
-        return await mailModule.GetMailListAsync();
+        var dbResult = await mailModule.GetMailListAsync();
+        if (dbResult == null)
+            return [];
+        
+        return dbResult.Values.ToList();
+    }
+
+    public async Task ReceivedMailRewardAsync(List<long> mailIds)
+    {
+        var currentServerTime = TimeZoneHelper.ServerTimeNow;
+        var receivedFailed =  new List<long>();
+        
+        var mailModule = GetModule<MailModule>();
+        var mailList =  await mailModule.GetMailListAsync();
+        
+        foreach (var mailUid in mailIds)
+        {
+            if (mailList.TryGetValue(mailUid, out var mailDbInfo) == false)
+            {
+                receivedFailed.Add(mailUid);
+                continue;
+            }
+
+            if (mailDbInfo.expiry_date.ToServerTime() > currentServerTime)
+            {
+                receivedFailed.Add(mailUid);
+                continue;
+            }
+
+            if (mailDbInfo.is_reward_received == 1)
+            {
+                receivedFailed.Add(mailUid);
+                continue;
+            }
+            
+            mailDbInfo.is_reward_received = 1;
+        }
     }
 }
