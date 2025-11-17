98ec692d6f8c8dbb5db2c3757425cf9bfa175ea5
heohyeon <doubleh86@gmail.com>
2025-11-17 10:09:36 +0900

    쿼리 버그 수정

--- DbContext/SharedContext/MySqlContext/QueryCommands/GetAccountInfoCommand.cs
+++ DbContext/SharedContext/MySqlContext/QueryCommands/GetAccountInfoCommand.cs
@@ -11,22 +11,22 @@ namespace DbContext.SharedContext.MySqlContext.QueryCommands;
 public class GetAccountInfoCommand(MySqlDapperServiceBase dbContext, MySqlTransaction transaction = null) 
     : QueryCommandBaseAsync<GetAccountDbResult, GetAccountDbResult>(dbContext, transaction)
 {
-    private const string _GetAccountLoginID = "SELECT A.account_id AS AccountId, ISNULL(A.login_id, '') AS LoginId, " +
-                                              "ISNULL(A.account_type, 0) AS AccountType," +
-                                              "ISNULL(A.game_db_uid, -1) AS GameDbUID, " +
-                                              "ISNULL(B.db_name, '') AS DbName, " +
-                                              "ISNULL(B.host, '') AS Host, " +
-                                              "ISNULL(B.port, 0) AS Port, " +
-                                              "ISNULL(B.db_login_id, '') AS DbLoginId, " +
-                                              "ISNULL(B.db_password, '') AS DbPassword, " +
-                                              "ISNULL(B.slave_db_name, '') AS SlaveDbName, " +
-                                              "ISNULL(B.slave_host, '') AS SlaveHost, " +
-                                              "ISNULL(B.slave_port, 0) AS SlavePort, " +
-                                              "ISNULL(B.slave_db_login_id, '') AS SlaveDbLoginId, " +
-                                              "ISNULL(B.slave_db_password, '') AS SlaveDbPassword, " +
+    private const string _GetAccountLoginID = "SELECT A.account_id AS AccountId, IFNULL(A.login_id, '') AS LoginId, " +
+                                              "IFNULL(A.account_type, 0) AS AccountType," +
+                                              "IFNULL(A.game_db_uid, -1) AS GameDbUID, " +
+                                              "IFNULL(B.db_name, '') AS DbName, " +
+                                              "IFNULL(B.host, '') AS Host, " +
+                                              "IFNULL(B.port, 0) AS Port, " +
+                                              "IFNULL(B.db_login_id, '') AS DbLoginId, " +
+                                              "IFNULL(B.db_password, '') AS DbPassword, " +
+                                              "IFNULL(B.slave_db_name, '') AS SlaveDbName, " +
+                                              "IFNULL(B.slave_host, '') AS SlaveHost, " +
+                                              "IFNULL(B.slave_port, 0) AS SlavePort, " +
+                                              "IFNULL(B.slave_db_login_id, '') AS SlaveDbLoginId, " +
+                                              "IFNULL(B.slave_db_password, '') AS SlaveDbPassword, " +
                                               "A.update_date as UpdateDate, A.create_date as CreateDate " +
-                                              "FROM dbo.account_info AS A LEFT JOIN " +
-                                              "dbo.game_db_info AS B ON A.game_db_uid = B.uid " +
+                                              "FROM account_info AS A LEFT JOIN " +
+                                              "game_db_info AS B ON A.game_db_uid = B.uid " +
                                               "WHERE A.login_id = @loginId;";
     public struct InParameters : IDbInParameters
     {
